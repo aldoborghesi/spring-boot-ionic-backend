1fa3f479856c85e810e82c732f57a680ec19e018
Aldo Borghesi <aldoborghesi@gmail.com>
2019-07-14 17:57:42 -0300

    Criando Respositorie e Service para Categoria

--- src/main/java/com/sly/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/sly/cursomc/resources/CategoriaResource.java
@@ -3,11 +3,15 @@ package com.sly.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sly.cursomc.domain.Categoria;
+import com.sly.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -18,7 +22,15 @@ public class CategoriaResource {
 		return "REST está funcionando";
 	}]*/
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	/*
 	public List<Categoria> listar() {
 		
 		Categoria cat1 = new Categoria(1, "Escritório");
@@ -30,5 +42,5 @@ public class CategoriaResource {
 		
 		return lista;
 	}
-
+*/    
 }
