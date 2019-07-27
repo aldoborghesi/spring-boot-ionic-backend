c23dd11d9d7c67fa1ad348361a8f4dbe4cb5c340
Aldo Borghesi <aldoborghesi@gmail.com>
2019-07-27 09:59:27 -0300

    Inserindo Categoria com POST

--- src/main/java/com/sly/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/sly/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,17 @@
 package com.sly.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URI;
+
+import javax.servlet.Servlet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sly.cursomc.domain.Categoria;
 import com.sly.cursomc.services.CategoriaService;
@@ -29,6 +32,15 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	// RequestBody transforma o JSOn em um objeto automaticamente	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		// O HTTP Exige uma resposta 201 e uma URI
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	/*
 	public List<Categoria> listar() {
